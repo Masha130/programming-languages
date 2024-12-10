344bae55f42201279a8908215858dbf655a2f5a0
Попова Мария <avior007@stud.vvsu.ru>
2024-12-10 17:50:48 +1000

    Добавьте файлы проекта.

---
+++ WpfApp6/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TreeViewItem_OnSelected(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem treeViewItem = (TreeViewItem)sender;
+            MessageBox.Show(messageBoxText: "выбран язык программирования: " + treeViewItem.Header.ToString(), caption: "выбор языка программирования", MessageBoxButton.OK, icon: MessageBoxImage.Information);
+        }
+    }
+}
